7c16c537d30e2415901853cb6808e9571d38c560
Arnold <Array0824@gmail.com>
2023-12-18 22:47:11 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,47 @@
+class invesment:
+    print ("Welcome to the return on property invesment calculator"):
+    print input ("What type of property?"):
+    print input ("what was the purchase price"):
+    print input ("what was the down payment?"):
+    print input ("what is your retal income"):
+
+    
+    def __income__(self,rental_income):
+        self.rental_income = rental_income
+        
+    def __expenses__(self,insurance,maintanace_fee,taxes,mortgage,others,total):
+        self.insurance = insurance
+        self.maintanace_fee = maintanace_fee
+        self.taxes = taxes 
+        self.mortgage = mortgage 
+        self.others = others 
+        self.total
+        
+        if self.insurance == int return int
+        if self.maintanace_fee == int return int
+        if self.taxes == int return int
+        if self.mortgage == int return int
+        if self.others == int return int
+        if self.total == int return int 
+
+        print self.total == ({insurance} + {maintanace_fee} + {taxes} + {mortgage} + {others})
+        
+    def __intitalandcurrentmonies__(self,downpayment,rehab,roofing)
+        self.rehab = rehab 
+        self.roofing = roofing 
+        self.downpayment = downpayment 
+
+        print "{downpayment}"
+        print "{roofing}"
+        print "{rehab}"
+
+
+    
+    def __cashflow__(self,):
+        self.rental_income -= self.total
+
+        print ("cash flow total {self.total}")
+        
+        
+        print input("quit"):
+        break
